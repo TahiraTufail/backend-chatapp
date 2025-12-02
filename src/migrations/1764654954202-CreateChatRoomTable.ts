f507db99fcba9d013dacf435c9a9d673670bacc5
TahiraTufail <tahiratufail.62@gmail.com>
2025-12-02 10:57:34 +0500

    implementing websocket

---
+++ src/migrations/1764654954202-CreateChatRoomTable.ts
@@ -0,0 +1,18 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateChatRoomTable1764654954202 implements MigrationInterface {
+    name = 'CreateChatRoomTable1764654954202'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "contact" DROP CONSTRAINT "FK_e7e34fa8e409e9146f4729fd0cb"`);
+        await queryRunner.query(`ALTER TABLE "contact" ALTER COLUMN "userId" DROP NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "contact" ADD CONSTRAINT "FK_e7e34fa8e409e9146f4729fd0cb" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "contact" DROP CONSTRAINT "FK_e7e34fa8e409e9146f4729fd0cb"`);
+        await queryRunner.query(`ALTER TABLE "contact" ALTER COLUMN "userId" SET NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "contact" ADD CONSTRAINT "FK_e7e34fa8e409e9146f4729fd0cb" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+    }
+
+}
